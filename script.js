23a9d70b967e448e81fb272e9ce6861125c453e2
kz <kennethlorenzg@gmail.com>
2025-12-14 22:37:41 -0500

    Add algorithm black/random fill color

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const SQUARECONTAINER = document.querySelector("#square-container");
+const BLACKBUTTON = document.querySelector("#black");
+const RANDOMBUTTON = document.querySelector("#random");
+
+let selectedColor = "Black";
+let isMouseDown = false;
 
 function generateSquares() {
   let dimension = 16;
@@ -13,17 +18,31 @@ function generateSquares() {
 
 generateSquares();
 let squares = document.querySelectorAll(".square");
-console.log(squares);
 
 function fillBlackColor(element) {
   element.classList.add("black");
 }
 
-let isMouseDown = false;
+function fillRandomColor(element) {
+  let rColorValue = Math.floor(Math.random() * 256);
+  let gColorValue = Math.floor(Math.random() * 256);
+  let bColorValue = Math.floor(Math.random() * 256);
+
+  let color = `rgb(${rColorValue} ${gColorValue} ${bColorValue}`;
+  element.style.backgroundColor = color;
+}
+
+function fillColor(square) {
+  if (selectedColor == "Black") {
+    fillBlackColor(square);
+  } else if (selectedColor == "Random") {
+    fillRandomColor(square);
+  }
+}
 
 squares.forEach((square) => {
   square.addEventListener("mousedown", (e) => {
-    fillBlackColor(square);
+    fillColor(square);
     isMouseDown = true;
     e.preventDefault();
   });
@@ -38,9 +57,30 @@ squares.forEach((square) => {
 squares.forEach((square) => {
   square.addEventListener("mouseover", () => {
     if (isMouseDown) {
-      fillBlackColor(square);
+      fillColor(square);
     } else {
       return;
     }
   });
+});
+
+function toggleActiveButton(activeButton, inActiveButton) {
+  activeButton.classList.toggle("active");
+  inActiveButton.classList.toggle("active");
+}
+
+BLACKBUTTON.addEventListener("click", () => {
+  if (BLACKBUTTON.classList.contains("active")) {
+    return;
+  }
+  toggleActiveButton(BLACKBUTTON, RANDOMBUTTON);
+  selectedColor = "Black";
+});
+
+RANDOMBUTTON.addEventListener("click", () => {
+  if (RANDOMBUTTON.classList.contains("active")) {
+    return;
+  }
+  toggleActiveButton(RANDOMBUTTON, BLACKBUTTON);
+  selectedColor = "Random";
 });
